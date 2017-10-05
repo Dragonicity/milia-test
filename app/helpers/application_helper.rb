920e5b0fdd97bbd49f9f63c2fd302f4500c52287
Mark-Nelson <mark.nelson@dragonicity.com>
2017-10-05 19:15:26 +0800

    CSS settings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+   def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      } 
+    ]
+  end
+
+  def nav_helper
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{'li'}><a href='#{item[:url]}' class='#{'nav-link'} #{active? item[:url]}'>#{item[:title]}</a></#{'li'}>"
+    end
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
 end
