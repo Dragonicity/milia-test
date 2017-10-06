35b8f5e619e037dfc5f1dcd2194a8c58e09b5c50
Mark-Nelson <mark.nelson@dragonicity.com>
2017-10-06 16:24:47 +0800

    Work on language selector

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,27 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SetLayoutDefaults
 
-  before_action :set_locale
+  before_action :set_i18n_locale_from_params
  
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
+  protected
 
-  def default_url_options
-    { locale: I18n.locale }
-  end
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
+
+    def set_i18n_locale_from_params
+      if params[:set_locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:set_locale])
+          I18n.locale = params[:set_locale]
+        else
+          flash.now[:notice] = 
+            "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
+      end
+    end
+
+    def default_url_options
+      { locale: I18n.locale }
+    end
 end
