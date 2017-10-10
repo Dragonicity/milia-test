60f957a196c67b4b53d55a15667ef8bd0bc23a69
Mark-Nelson <mark.nelson@dragonicity.com>
2017-10-10 19:30:19 +0800

    Added topics seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,19 @@ user.last_name = "Nelson"
 user.roles = "site_admin"
 user.save!
 
+Topic.create!(title: "Retreats")
+Topic.create!(title: "Retreat Design")
+Topic.create!(title: "Services")
+Topic.create!(title: "Products")
+
 12.times do |blog|
   Blog.create!(
     title:      "Blog Post #{blog}",
     body:       "My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?",
     thumb_nail: "http://placehold.it/350x200",
     main_image: "http://placehold.it/600x400",
-    user_id:    user.id 
+    user_id:    user.id,
+    topic_id: Topic.first.id
   )
 end
 
