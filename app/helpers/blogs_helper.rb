af110783bdcb8b7ad39f09685b55e2ef98494685
Mark-Nelson <mark.nelson@dragonicity.com>
2017-10-16 12:58:18 +0800

    Replace blog status toggle with buttons

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -3,7 +3,12 @@ module BlogsHelper
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
   end
 
+  
+  def blog_status_text blog
+    blog.draft? ? t(:publish) : t(:unpublish)
+  end
+
   def blog_status_colour blog
-    'color: red;'if blog.draft?
+    blog.draft? ? 'color: red;' :  'color: blue'
   end
 end
