a90897f2d64154f5cd7c0e869751f646182660c0
Mark-Nelson <mark.nelson@dragonicity.com>
2017-10-16 13:47:39 +0800

    Added topics scope for blog sidebar

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,6 +2,8 @@ class Topic < ApplicationRecord
   has_many :blogs
   validates_presence_of :title
 
+  scope :title_order, -> { order("topics.title ASC") }
+
   def self.with_blogs
     includes(:blogs).where.not(blogs: { id: nil } )
   end
