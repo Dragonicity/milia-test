5934d367712e9e9739d13d9324e7045ae512425f
Mark-Nelson <mark.nelson@dragonicity.com>
2017-10-17 17:00:27 +0800

    Added uuid for purchase

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017082208) do
+ActiveRecord::Schema.define(version: 20171017085242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20171017082208) do
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uuid"
   end
 
   create_table "topics", force: :cascade do |t|
